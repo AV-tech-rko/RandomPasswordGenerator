12db4cccf55bc18cea08326e57dc15cdbb576ddf
AV-tech-rko <63596408+AV-tech-rko@users.noreply.github.com>
2020-04-28 15:24:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import RandomPasswordGenerator as MyPassword
 
 while 1:
   user_pass_length = int(input("Enter password length: \n"))
-  print(MyPassword.GeneratePassword
+  print(MyPassword.GeneratePassword(user_pass_length)
+  userreply = input("Do you want more passwords?  y/n ")
+  if userreply == "y":
+      continue
+  elif userreply == "n":
+	  break	
